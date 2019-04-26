5f8d6067d77c11dc02427047fafb6e99b8f8e0f3
vpaniego <vanesa.paniegos@alumnos.upm.es>
2019-04-26 21:59:28 +0200

    issue #5. Create HotelRepositoryIT with test failed

---
+++ src/test/java/es/upm/miw/repositories/HotelRepositoryIT.java
@@ -0,0 +1,15 @@
+package es.upm.miw.repositories;
+
+import es.upm.miw.TestConfig;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@TestConfig
+public class HotelRepositoryIT {
+
+    @Test
+    void testReadAllHotels(){
+        assertTrue(false);
+    }
+}
