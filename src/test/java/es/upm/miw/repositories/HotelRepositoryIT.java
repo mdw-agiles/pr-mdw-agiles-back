9d80995e8850eaa482f6ecfeea0396ac1431bae4
vpaniego <vanesa.paniegos@alumnos.upm.es>
2019-04-26 22:13:28 +0200

    issue #5. Refactor HotelRepositoryIT

--- src/test/java/es/upm/miw/repositories/HotelRepositoryIT.java
+++ src/test/java/es/upm/miw/repositories/HotelRepositoryIT.java
@@ -1,15 +1,46 @@
 package es.upm.miw.repositories;
 
 import es.upm.miw.TestConfig;
+import es.upm.miw.documents.Hotel;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @TestConfig
 public class HotelRepositoryIT {
 
-    @Test
-    void testReadAllHotels(){
-        assertTrue(false);
+    @Autowired
+    private HotelRepository hotelRepository;
+
+    private Hotel hotel;
+
+    @BeforeEach
+    void seedDB() {
+        this.hotel = new Hotel();
+        this.hotel.setName("Hotel Riu Plaza España");
+        this.hotelRepository.save(hotel);
     }
+
+    @Test
+    void testReadAll() {
+        assertFalse(this.hotelRepository.findAll().isEmpty());
+    }
+
+    @Test
+    void testUpdateHotel(){
+        String hotelName = "Hotel Riu Plaza España";
+        assertEquals(hotelName, this.hotel.getName());
+        this.hotel.setName("Hotel Riu Cancun");
+        this.hotelRepository.save(hotel);
+        assertNotEquals(hotelName, this.hotel.getName());
+    }
+
+    @AfterEach
+    void testRemoveHotel(){
+        this.hotelRepository.delete(hotel);
+    }
+
 }
