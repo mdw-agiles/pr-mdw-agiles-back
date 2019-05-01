67f64a9df376059f084950f4e5582cd7779a7c6f
robertome <roberto.martin.escribano@alumnos.upm.es>
2019-05-01 11:09:42 +0200

    issue #7 Added first Test and REST Resource "rooms/{id}/booked-dates/{date}" (for date format yyyy-MM-dd)

--- src/main/java/es/upm/miw/rest_controllers/RoomResource.java
+++ src/main/java/es/upm/miw/rest_controllers/RoomResource.java
@@ -1,31 +1,46 @@
 package es.upm.miw.rest_controllers;
 
+import es.upm.miw.business_controllers.ReservationController;
 import es.upm.miw.business_controllers.RoomController;
 import es.upm.miw.dtos.RoomDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RestController
 @RequestMapping(RoomResource.ROOM)
 public class RoomResource {
 
-    public static final String ROOM = "/room";
+    public static final String ROOM = "/rooms";
     public static final String SEARCH = "/search";
+
+    private static final String BOOKED_DATES = "/booked-dates";
+    private static final String ROOM_ID = "/{roomId}";
+    private static final String DATE_ID = "/{date}";
+    public static final String ROOM_BOOKED_DATES = ROOM_ID + BOOKED_DATES + DATE_ID;
+
 
     @Autowired
     private RoomController roomController;
+    @Autowired
+    private ReservationController reservationController;
 
     @GetMapping
-    public List<RoomDto> readAll() { return this.roomController.readAll(); }
+    public List<RoomDto> readAll() {
+        return this.roomController.readAll();
+    }
 
     @GetMapping(value = SEARCH)
     public List<RoomDto> searchByHotel(@RequestParam() String hotelName) {
         return this.roomController.searchByHotel(hotelName);
+    }
+
+    @GetMapping(value = ROOM_BOOKED_DATES)
+    public Date[] calculateStockPrediction(@PathVariable String roomId,
+                                           @PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") Date date) {
+        return new Date[] {};
     }
 }
--- src/test/java/es/upm/miw/rest_controllers/RoomResourceIT.java
+++ src/test/java/es/upm/miw/rest_controllers/RoomResourceIT.java
@@ -5,9 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ApiTestConfig
 public class RoomResourceIT {
@@ -16,12 +20,12 @@ public class RoomResourceIT {
     private RestService restService;
 
     @Test
-    void testReadAll(){
+    void testReadAll() {
         List<RoomDto> roomDtoList = Arrays.asList(this.restService.loginAdmin()
                 .restBuilder(new RestBuilder<RoomDto[]>()).clazz(RoomDto[].class)
                 .path(RoomResource.ROOM)
                 .get().build());
-        assertFalse( roomDtoList.isEmpty());
+        assertFalse(roomDtoList.isEmpty());
     }
 
     @Test
@@ -44,5 +48,17 @@ public class RoomResourceIT {
                 .param("hotelName", "NH Madrid Centro")
                 .get().build());
         assertFalse(roomDtoList.isEmpty());
+    }
+
+    @Test
+    void givenRoomAndDate_thenReturnBookedDates() {
+        List<Date> bookedDates = Arrays.asList(this.restService.loginAdmin()
+                .restBuilder(new RestBuilder<Date[]>()).clazz(Date[].class)
+                .path(RoomResource.ROOM).path(RoomResource.ROOM_BOOKED_DATES)
+                .expand("r-5cbc2adec2e17403fb397c6b")
+                .expand("2019-04-21")
+                .get().build());
+        assertFalse(bookedDates.isEmpty());
+        assertThat(bookedDates.size(), is(2));
     }
 }
